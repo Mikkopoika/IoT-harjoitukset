bfe84ca4968431f9ca75a9cc1ab6e476b71e3fbe
Mikkopoika <mikko.muilu@gmail.com>
2019-01-21 00:55:49 +0200

    2.2 lisätty

--- harjoitus2/koodi22.py
+++ harjoitus2/koodi22.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO
+import time
 
-#Asetetaan nappi sisääntuloksi pinniin 5
+GPIO.setmode (GPIO.BCM)
+
+#Asetetaan nappi sisaantuloksi pinniin 5
 GPIO.setup(5, GPIO.IN)
 
 #Asetetaan punaisen ledin ulostuloksi pinni 6
-GPIO.setup(pin, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
 
-if GPIO.input(5)
-	GPIO.output(6) = true
-else 
-	GPIO.output(6) = false
 
+loppu = time.time() + 10
+while time.time() < loppu:
+	GPIO.output(6, GPIO.input (5))
+	time.sleep (0.1) # ilman tata prossukaytto 100%
+
+GPIO.cleanup ()
