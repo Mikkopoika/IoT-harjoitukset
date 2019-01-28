175f50bfaf6e19dae4f2f5834340db0a2ea730bb
Mikkopoika <mikko.muilu@gmail.com>
2019-01-28 10:52:04 +0200

    kuva ja palvelin

---
+++ harjoitus3/koodi33.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+#Ottaa kuvan 10 sekunnin valein palvelimen html-kansioon.
+while (0<1):
+	camera.capture('/home/pi/Pictures/image.jpg')
+	sleep(10)
+	
+#ottaa huonolaatuista videota 15s.
+#camera.resolution = (128,128)
+#camera.start_recording('/home/pi/video.h264')
+#sleep(15)
+#camera.stop_recording()
+	
+	
