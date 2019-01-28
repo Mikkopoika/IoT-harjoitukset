5b42284599022e39396d876d200981235cfc8f06
Mikkopoika <mikko.muilu@gmail.com>
2019-01-28 10:53:59 +0200

    video

--- harjoitus3/koodi33.py
+++ harjoitus3/koodi33.py
@@ -6,14 +6,14 @@ from time import sleep
 camera = PiCamera()
 
 #Ottaa kuvan 10 sekunnin valein palvelimen html-kansioon.
-while (0<1):
-	camera.capture('/home/pi/Pictures/image.jpg')
-	sleep(10)
+#while (0<1):
+#	camera.capture('/home/pi/Pictures/image.jpg')
+#	sleep(10)
 	
 #ottaa huonolaatuista videota 15s.
-#camera.resolution = (128,128)
-#camera.start_recording('/home/pi/video.h264')
-#sleep(15)
-#camera.stop_recording()
+camera.resolution = (128,128)
+camera.start_recording('/home/pi/video.h264')
+sleep(15)
+camera.stop_recording()
 	
 	
